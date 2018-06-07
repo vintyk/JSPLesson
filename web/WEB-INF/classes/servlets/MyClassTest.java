d126d4aee2f5502d57b1a734ee5ba57fe13cd24e
vinty <vinty1978@gmail.com>
2018-06-07 21:25:19 +0300

    молодец в классе Ярослава

--- web/WEB-INF/classes/servlets/MyClassTest.java
+++ web/WEB-INF/classes/servlets/MyClassTest.java
@@ -14,6 +14,6 @@ import java.io.IOException;
 public class MyClassTest extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("МОЛОДЕЦ!");
     }
 }
